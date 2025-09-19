0fc73416a3f1194554a98f69c4aa6a515530b395
gustavo <gimuller@nelogica.com.br>
2025-09-19 00:20:46 -0300

    TicTacToe

--- Test Driven Java Development/TicTacToe/src/main/java/org/example/TicTacToe.java
+++ Test Driven Java Development/TicTacToe/src/main/java/org/example/TicTacToe.java
@@ -1,4 +1,78 @@
 package org.example;
 
 public class TicTacToe {
+
+    private Character[][] board =
+            {{'\0', '\0', '\0'},
+             {'\0', '\0', '\0'},
+             {'\0', '\0', '\0'}};
+
+    private char player = 'X';
+
+    public char nextPlayer() {
+        return player;
+    }
+
+    public String play(int x, int y) {
+        checkAxis(x);
+        checkAxis(y);
+        place(x, y);
+
+        if (isDraw()) {
+            return "The result is draw";
+        }
+
+        if (isWin(x, y)) {
+            return "Winner is " + player;
+        }
+
+        switchPlayer();
+        return "Not ended";
+    }
+
+    private void checkAxis(int k) {
+        if (k < 1 || k > 3) {
+            throw new RuntimeException("Out of bounds");
+        }
+    }
+
+    private void place(int x, int y) {
+        if  (board[x - 1][y - 1] != '\0') {
+            throw new RuntimeException("Already placed!");
+        }
+        board[x - 1][y - 1] = player;
+    }
+
+    private void switchPlayer() {
+        player = player == 'X' ? 'O' : 'X';
+    }
+
+    private boolean isWin(int x, int y) {
+        int total = player * 3;
+        char vertical, horizontal, diagonal1, diagonal2;
+        vertical = horizontal = diagonal1 = diagonal2 = '\0';
+
+        for (int i = 0; i < 3; i++) {
+            vertical += board[x - 1][i];
+            horizontal += board[i][y - 1];
+            diagonal1 += board[i][i];
+            diagonal2 += board[i][3 - i - 1];
+        }
+
+        if (vertical == total || horizontal == total || diagonal1 == total || diagonal2 == total) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isDraw() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '\0') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
--- Test Driven Java Development/TicTacToe/src/test/java/TicTacToeSpec.java
+++ Test Driven Java Development/TicTacToe/src/test/java/TicTacToeSpec.java
@@ -1,2 +1,124 @@
+import org.example.TicTacToe;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TicTacToeSpec {
+
+    private TicTacToe game;
+
+    @BeforeEach
+    public void setup() {
+        game = new TicTacToe();
+    }
+
+    @Test
+    public void whenXOutsideBoardThenRuntimeException() {
+        assertThrows(RuntimeException.class, () -> {
+            game.play(0, 1);
+        });
+        assertThrows(RuntimeException.class, () -> {
+            game.play(4, 1);
+        });
+    }
+
+    @Test
+    public void whenYOutsideBoardThenRuntimeException() {
+        assertThrows(RuntimeException.class, () -> {
+            game.play(1, 0);
+        });
+        assertThrows(RuntimeException.class, () -> {
+            game.play(1, 4);
+        });
+    }
+
+    @Test
+    public void whenOccupiedPieceThenRuntimeException() {
+        game.play(1, 1);
+        assertThrows(RuntimeException.class, () -> {
+            game.play(1, 1);
+        });
+    }
+
+    @Test
+    public void whenStartThenXTurn() {
+        assertEquals('X', game.nextPlayer());
+    }
+
+    @Test
+    public void whenXPlaysThenYTurn() {
+        game.play(1, 1);
+        assertEquals('O', game.nextPlayer());
+    }
+
+    @Test
+    public void whenYPlaysThenXTurn() {
+        game.play(1, 1);
+        game.play(2, 1);
+        assertEquals('X', game.nextPlayer());
+    }
+
+    @Test
+    public void whenPlayThenNoWinner() {
+        game.play(1, 1);
+        game.play(2, 1);
+        var ended = game.play(3, 1);
+        assertEquals("Not ended", ended);
+    }
+
+    @Test
+    public void whenPlayWholeHorizontalThenWinner() {
+        game.play(1, 1);
+        game.play(1, 2);
+        game.play(2, 1);
+        game.play(1, 3);
+        var result = game.play(3, 1);
+        assertEquals("Winner is X", result);
+    }
+
+    @Test
+    public void whenPlayWholeVerticalThenWinner() {
+        game.play(1, 1);
+        game.play(3, 3);
+        game.play(1, 2);
+        game.play(2, 2);
+        var result = game.play(1, 3);
+        assertEquals("Winner is X", result);
+    }
+
+    @Test
+    public void whenPlayWholeDiagonalThenWinner() {
+        game.play(1, 1);
+        game.play(1, 2);
+        game.play(2, 2);
+        game.play(1, 3);
+        var result = game.play(3, 3);
+        assertEquals("Winner is X", result);
+    }
+
+    @Test
+    public void whenPlayWholeDiagonalThenWinner2() {
+        game.play(1, 2);
+        game.play(1, 3);
+        game.play(2, 3);
+        game.play(3, 1);
+        game.play(3, 3);
+        var result = game.play(2, 2);
+        assertEquals("Winner is O", result);
+    }
+
+    @Test
+    public void whenBoardFullThenDraw() {
+        game.play(1, 1);
+        game.play(1, 2);
+        game.play(1, 3);
+        game.play(2, 1);
+        game.play(2, 3);
+        game.play(2, 2);
+        game.play(3, 1);
+        game.play(3, 3);
+        String actual = game.play(3, 2);
+        assertEquals("The result is draw", actual);
+    }
 }
